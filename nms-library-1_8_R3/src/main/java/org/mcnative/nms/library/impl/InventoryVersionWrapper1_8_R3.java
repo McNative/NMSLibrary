95d763190e04a45491d038ffa2f3005ad5f612c2
Fridious <philippfriedhoff@outlook.de>
2021-03-07 19:03:57 +0100

    Implemented inventory version wrapper for 1.8-R3

---
+++ nms-library-1_8_R3/src/main/java/org/mcnative/nms/library/impl/InventoryVersionWrapper1_8_R3.java
@@ -0,0 +1,82 @@
+package org.mcnative.nms.library.impl;
+
+import net.minecraft.server.v1_8_R3.*;
+import org.bukkit.craftbukkit.v1_8_R3.entity.CraftPlayer;
+import org.bukkit.craftbukkit.v1_8_R3.event.CraftEventFactory;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.Inventory;
+import org.mcnative.nms.library.api.InventoryVersionWrapper;
+
+public class InventoryVersionWrapper1_8_R3 implements InventoryVersionWrapper {
+
+    @Override
+    public int getNextContainerId(Player player, Object container) {
+        return toNMS(player).nextContainerCounter();
+    }
+
+    @Override
+    public void handleInventoryCloseEvent(Player player) {
+        CraftEventFactory.handleInventoryCloseEvent(toNMS(player));
+    }
+
+    @Override
+    public void sendPacketOpenWindow(Player player, int containerId, String guiTitle) {
+        toNMS(player).playerConnection.sendPacket(new PacketPlayOutOpenWindow(containerId, "minecraft:anvil", new ChatMessage(Blocks.ANVIL.a() + ".name")));
+    }
+
+    @Override
+    public void sendPacketCloseWindow(Player player, int containerId) {
+        toNMS(player).playerConnection.sendPacket(new PacketPlayOutCloseWindow(containerId));
+    }
+
+    @Override
+    public void setActiveContainerDefault(Player player) {
+        toNMS(player).activeContainer = toNMS(player).defaultContainer;
+    }
+
+    @Override
+    public void setActiveContainer(Player player, Object container) {
+        toNMS(player).activeContainer = (Container) container;
+    }
+
+    @Override
+    public void setActiveContainerId(Object container, int containerId) {
+        ((Container) container).windowId = containerId;
+    }
+
+    @Override
+    public void addActiveContainerSlotListener(Object container, Player player) {
+        ((Container) container).addSlotListener(toNMS(player));
+    }
+
+    @Override
+    public Inventory toBukkitInventory(Object container) {
+        return ((Container) container).getBukkitView().getTopInventory();
+    }
+
+    @Override
+    public Object newContainerAnvil(Player player, String guiTitle) {
+        return new AnvilContainer(toNMS(player));
+    }
+
+    private EntityPlayer toNMS(Player player) {
+        return ((CraftPlayer) player).getHandle();
+    }
+
+    private class AnvilContainer extends ContainerAnvil {
+
+        public AnvilContainer(EntityHuman entityhuman) {
+            super(entityhuman.inventory, entityhuman.world, new BlockPosition(0, 0, 0), entityhuman);
+        }
+
+        @Override
+        public boolean a(EntityHuman human) {
+            return true;
+        }
+
+        @Override
+        public void b(EntityHuman entityhuman) {
+        }
+
+    }
+}
